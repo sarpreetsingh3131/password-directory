c8b1f23f802a2c9c3b437030607ca4cf2cd83933
sarpreetsingh3131 <sunnybuttar31@gmail.com>
2017-10-28 14:54:08 +0200

    setup for electron

---
+++ main.js
@@ -0,0 +1,18 @@
+const electron = require('electron')
+var app = electron.app
+var BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+const start = () => {
+  mainWindow = new BrowserWindow({width: 800, height: 600, minWidth: 500, minHeight: 300})
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.webContents.openDevTools()
+  mainWindow.on('closed', () => { mainWindow = null })
+}
+
+app.on('ready', start)
+
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') { app.quit() } })
+
+app.on('activate', () => { if (mainWindow === null) { start() } })
