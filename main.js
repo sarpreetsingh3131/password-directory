09261c8ae0fa1eb588c54e60f79f0b7306398694
sarpreetsingh3131 <sunnybuttar31@gmail.com>
2017-10-28 14:56:01 +0200

    added assets

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let mainWindow
 const start = () => {
   mainWindow = new BrowserWindow({width: 800, height: 600, minWidth: 500, minHeight: 300})
   mainWindow.loadURL(`file://${__dirname}/index.html`)
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
   mainWindow.on('closed', () => { mainWindow = null })
 }
 
